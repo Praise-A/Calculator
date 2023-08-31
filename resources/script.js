cc64cf64bca07f6edf9cd9a7189e0b10c3b32570
Praise-A <praise.amaraegbulam@stu.cu.edu.ng>
2023-08-31 22:41:41 +0100

    Initial commit: Create index, style and script files

---
+++ resources/script.js
@@ -0,0 +1,18 @@
+let a = document.querySelector("");
+let b = document.querySelector("");
+
+let operator = document.querySelector("");
+
+let add = (a, b) => {};
+
+let subtract = (a, b) => {};
+
+let multiply = (a, b) => {};
+
+let divide = (a, b) => {};
+
+let operate = (operator, a, b) => {};
+
+let display = () => {
+  let displayValue;
+};
