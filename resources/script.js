00887195ab076c56d2ca2a62458e5267dc8dde97
Praise-A <praise.amaraegbulam@stu.cu.edu.ng>
2023-09-09 19:07:33 +0100

    Modify functions for displaying calculator numbers on click

--- resources/script.js
+++ resources/script.js
@@ -1,18 +1,80 @@
-let a = document.querySelector("");
-let b = document.querySelector("");
+let firstNum = "";
+let secondNum = "";
 
-let operator = document.querySelector("");
+let displayMain = document.querySelector("#displayMain");
 
-let add = (a, b) => {};
+let clearBtn = document.getElementById("clear");
+let deleteBtn = document.querySelector("#delete");
 
-let subtract = (a, b) => {};
+let equalsBtn = document.querySelector("#equalsSign");
 
-let multiply = (a, b) => {};
+let numbersArr = document.querySelectorAll(".calc-number");
+let operatorsArr = document.querySelectorAll(".calc-operator");
 
-let divide = (a, b) => {};
+//Display Numbers
+numbersArr.forEach((num) => {
+  num.addEventListener("click", () => displayNum(num.textContent));
+});
 
-let operate = (operator, a, b) => {};
-
-let display = () => {
-  let displayValue;
+let displayNum = (param) => {
+  if (displayMain.textContent == "0") {
+    displayMain.textContent = "";
+  }
+  displayMain.textContent += param;
 };
+
+//Display Operators
+operatorsArr.forEach((operator) => {
+  operator.addEventListener("click", () =>
+    displayOperator(operator.textContent)
+  );
+});
+
+let displayOperator = (operator) => {
+  displayMain.textContent += operator;
+};
+
+//Clear Button
+clearBtn.addEventListener("click", clearDisplay);
+
+function clearDisplay() {
+  displayMain.textContent = "0";
+  console.log("Cleared!");
+}
+
+let add = (a, b) => {
+  return a + b;
+};
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let divide = (a, b) => {
+  return a / b;
+};
+
+//Equals to Function
+equalsBtn.addEventListener("click", () => operate());
+
+let operate = (operator, firstNum, secondNum) => {
+  if (operator == "+") {
+    add(firstNum, secondNum);
+  } else if (operator == "-") {
+    subtract(firstNum, secondNum);
+  } else if (operator == "*") {
+    multiply(firstNum, secondNum);
+  } else if (operator == "/") {
+    divide(firstNum, secondNum);
+  } else {
+    return null;
+  }
+};
+
+// let display = () => {
+//   displayValue = operate();
+// };
